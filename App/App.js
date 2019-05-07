e6040dcd22e4eb653c615932409e5193fc326427
Mitchell Pebbles <mitchell.jeffrey.pebbles@gmail.com>
2019-05-07 14:54:31 -0700

    added basic routing

--- App/App.js
+++ App/App.js
@@ -1,21 +1,13 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, View } from 'react-native';
+import Routes from './Routes.js'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hello, world! This is for testing</Text>
-      </View>
-    );
-  }
+class App extends Component {
+   render() {
+      return (
+         <Routes />
+      )
+   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
+AppRegistry.registerComponent('App', () => App)
