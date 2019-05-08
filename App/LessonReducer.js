c18b59e11af13262a64b9e0e3020ac7ee3e496ef
Mitchell Pebbles <mitchell.jeffrey.pebbles@gmail.com>
2019-05-07 20:32:27 -0700

    added FlatList

--- App/LessonReducer.js
+++ App/LessonReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 
 const INITIAL_STATE = {
-  current: [],
-  possible: []
+  jsonFiles: ['Lesson 1', 'Lesson 2', 'Lesson 3', 'Lesson 4', 'Lesson 5',
+                'Lesson 6', 'Lesson 7', 'Lesson 8', 'Lesson 9', 'Lesson 10',
+                 'Lesson 11', 'Lesson 12', 'Lesson 13', 'Lesson 14',
+                 'Lesson 15', 'Lesson 16'],
 };
 
 const lessonReducer = (state = INITIAL_STATE, action) => {
--- App/pages/Home.js
+++ App/pages/Home.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { TouchableOpacity, Text, View } from "react-native";
+import {
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  View,
+  Button,
+  FlatList
+} from "react-native";
 import { Actions } from "react-native-router-flux";
 import { connect } from "react-redux";
 
@@ -11,15 +18,35 @@ const goToPlaceHolder = () => {
 class Home extends React.Component {
   render() {
     return (
-      <View>
-        <TouchableOpacity style={{ margin: 128 }} onPress={goToPlaceHolder}>
-          <Text>This is HOME!</Text>
-        </TouchableOpacity>
-        <Text> We have {this.props.lessons.current.length} lessons! </Text>
+      <View style={styles.container}>
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          data={this.props.lessons.jsonFiles}
+          renderItem={({ item }) => (
+            <Text style={styles.item} key={item}>
+              {item}
+            </Text>
+          )}
+        />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+    textAlign: "center"
+  }
+});
 
 const mapStateToProps = state => {
   const { lessons } = state;
@@ -27,4 +54,16 @@ const mapStateToProps = state => {
 };
 
 export default connect(mapStateToProps)(Home);
-//export default Home
+
+//<TouchableOpacity style={{ margin: 128 }} onPress={goToPlaceHolder}>
+//  <Text>This is HOME!</Text>
+//</TouchableOpacity>
+
+//{this.props.lessons.jsonFiles.map((fileName, index) => (
+//  <Text key={fileName}>{fileName}</Text>
+//<Button
+//  key={fileName}
+//  title={`Text ${fileName}`}
+//  onPress={() => {} } //this.props.screenProps.addFriend(index)}
+///>
+//))}
